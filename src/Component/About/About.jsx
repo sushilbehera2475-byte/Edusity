86c6c7105bb4ce9ff5bc2d3c7fba2614bae69d69
Sushil Kumar Behera <sushilbehera2475@gmail.com>
2026-06-10 11:39:52 +0530

    change the file name

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import './About.css'
-import playIcon from '../../Assets/play-icon.png'
-import aboutImg from '../../Assets/university.jpg'
+import React from "react";
+import "./About.css";
+import playIcon from "../../assets/play-icon.png";
+import aboutImg from "../../assets/university.jpg";
 const About = () => {
   return (
-    <div className='about'>
+    <div className="about">
       <div className="about-left">
-        <img src={aboutImg} alt="" className='about-img' />
-        <img src={playIcon} alt="" className='play-icon' />
+        <img src={aboutImg} alt="" className="about-img" />
+        <img src={playIcon} alt="" className="play-icon" />
       </div>
       <div className="about-right">
         <h3>ABOUT UNIVERSITY</h3>
         <h2>Nurturing Tomorrow's Leaders Today</h2>
-        <p>A university is an institution of higher learning
-           where students pursue advanced education, research, and professional
-            training.</p>
-        <p>It offers a wide range of academic programs across discip
-            ines such as science, arts, commerce, engineering, medicine, and law.</p>
-        <p>Universities not only provide knowledge but also encourage critical thinking,
-              creativity, and innovation, preparing students for careers and lifelong learning</p>
+        <p>
+          A university is an institution of higher learning where students
+          pursue advanced education, research, and professional training.
+        </p>
+        <p>
+          It offers a wide range of academic programs across discip ines such as
+          science, arts, commerce, engineering, medicine, and law.
+        </p>
+        <p>
+          Universities not only provide knowledge but also encourage critical
+          thinking, creativity, and innovation, preparing students for careers
+          and lifelong learning
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
