b835057b9b0edfe40efe4969d5f7f10558a4a405
Sushil Kumar Behera <sushilbehera2475@gmail.com>
2026-06-20 12:10:51 +0530

    Add new file

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,84 +1,44 @@
-import React from 'react'
-import './Contact.css'
-import message from '../../assets/message-ico.webp'
-import email from '../../assets/email-icon.webp'
-import phone from '../../assets/phone-icon.webp'
-import location from '../../assets/location-icon.webp'
-
+import React from "react";
+import "./Contact.css";
+import Login from "./Login";
+import message from "../../assets/message-ico.webp";
+import email from "../../assets/email-icon.webp";
+import phone from "../../assets/phone-icon.webp";
+import location from "../../assets/location-icon.webp";
 
 const Contact = () => {
   return (
-    <div className='contact'>
+    <div className="contact">
       <div className="contact-col">
-        <h3>send us a message<img src={message} alt="" /></h3>
-        
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
-        <ul>
-            <li><img src={email} alt="" />sushil210@gmail.com</li>
-            <li><img src={phone} alt="" />+91 6372749567</li>
-            <li><img src={location} alt="" />143, Badaraghunathpur, Bhubaneswer, India</li>
+        <h3>
+          send us a message
+          <img src={message} alt="" />
+        </h3>
 
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+          voluptate.
+        </p>
+        <ul>
+          <li>
+            <img src={email} alt="" />
+            sushil210@gmail.com
+          </li>
+          <li>
+            <img src={phone} alt="" />
+            +91 6372749567
+          </li>
+          <li>
+            <img src={location} alt="" />
+            143, Badaraghunathpur, Bhubaneswer, India
+          </li>
         </ul>
       </div>
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form action="#" method="POST" className="space-y-6">
-            <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-          <p className="mt-10 text-center text-sm/6 text-gray-400">
-            Not a member?{' '}
-            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
-              Start a 14 day free trial
-            </a>
-          </p>
+      <div>
+        <Login />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <App />
-</BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
